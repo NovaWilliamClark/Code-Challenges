43d23f72a4c9bcab1d685d790304dde45eda16f0
William Clark <111835542+NovaWilliamClark@users.noreply.github.com>
2022-12-05 16:12:52 +1300

    Update Solution.cpp

--- Quaternion Parsing/Solution.cpp
+++ Quaternion Parsing/Solution.cpp
@@ -4,35 +4,50 @@
 
 std::array<float, 4> ParseQuaternion(std::string t_quaternion)
 {
-  // Parse in a quaternion as 
+    // Store regex match Results
     std::smatch matches;
   
-    auto g = [](const std::string& t_letter, const std::string& t_equation, std::smatch& t_matches) {
+    // Lambda function that will locate an imaginary number using a letter as a parameter (ex. "i" will find -2i)).
+    // If no value is assigned a signed `1` is returned. If no value is matched `0` is returned
+    auto g = [](const std::string& t_letter, const std::string& t_equation, std::smatch& t_matches)
+    {
+        // Pattern for getting imaginary number coefficient with signage
         const std::regex pattern(R"((-?)([ (0-9).]+)?)" + t_letter);
         std::string result;
+      
+        // Search for match
         if (std::regex_search(t_equation, t_matches, pattern))
         {
             if (t_matches[2].matched && t_matches[2] != " ")
             {
+                // If the match contains a numeric value, remove any spaces, then
+                // concatenate the signage to the value;
                 auto matchStr = t_matches[2].str();
                 matchStr.erase(remove_if(matchStr.begin(), matchStr.end(), isspace), matchStr.end());
                 result = t_matches[1].str() + matchStr;
             }
             else
             {
+                // If the match does not contains a numeric value,
+                // concatenate signage and assume coefficient is 1
                 result = t_matches[1].str() + "1";
             }
         }
         else
         {
+            // no match value shall be 0.
             result = "0";
         }
-        return stof(result);
+        return std::stof(result);
     };
-
+  
+    // Pattern for getting the w component with signage (real component)
     std::regex realCompPattern("(-?)([0-9.]+)*[^ijk]([+]|-|$)");
+  
+    // Search for match
     std::regex_search(t_quaternion, matches, realCompPattern);
-
+  
+    // Store real component
     float w;
     if (!matches.empty())
     {
@@ -42,10 +57,13 @@ std::array<float, 4> ParseQuaternion(std::string t_quaternion)
     {
         w = 0;
     }
+  
+    // retrieve values from Lambda Function
     auto i = g("i", t_quaternion, matches);
     auto j = g("j", t_quaternion, matches);
     auto k = g("k", t_quaternion, matches);
 
+    // store & return the resulting quaternion coeffcients
     std::array<float, 4> values = { w, i, j, k };
   return values;
 }
